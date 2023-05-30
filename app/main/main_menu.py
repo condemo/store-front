55685d4d3ba5cea2badcd414d9745be64d845a5b
Gustavo de los Santos <gusleo94@gmail.com>
2023-05-30 20:02:47 +0200

    feat: menu buttons added

--- app/main/main_menu.py
+++ app/main/main_menu.py
@@ -3,8 +3,17 @@ import customtkinter as ctk
 
 class MainMenu(ctk.CTkFrame):
     def __init__(self, parent) -> None:
-        super().__init__(master=parent)
+        super().__init__(master=parent, width=50)
         self.parent = parent
         self.pack_propagate(False)
 
-        self.pack()
+        self.menu_btn_frame = ctk.CTkFrame(self, fg_color="transparent")
+
+        # TODO: Implemetar correctamente los botones por separado
+        [ctk.CTkButton(
+            self.menu_btn_frame, text=f"{i+1}", width=40)
+            .pack(pady=5) for i in range(5)]
+
+        self.menu_btn_frame.pack(expand=True)
+
+        self.pack(side="left", fill="y")
