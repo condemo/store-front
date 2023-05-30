c023435e163e4ee0760d318634c2a399c005c830
Gustavo de los Santos <gusleo94@gmail.com>
2023-05-30 18:26:46 +0200

    feat: initial main_menu and product frame

---
+++ app/products/product_views.py
@@ -0,0 +1,10 @@
+import customtkinter as ctk
+
+
+class ProductsView(ctk.CTkFrame):
+    def __init__(self, parent) -> None:
+        super().__init__(master=parent)
+        self.parent = parent
+        self.pack_propagate(False)
+
+        self.pack()
