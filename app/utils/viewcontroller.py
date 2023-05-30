18cd62077c093e1c0e4c00b6b751a085989e1d56
Gustavo de los Santos <gusleo94@gmail.com>
2023-05-30 20:30:35 +0200

    feat: view controller added

---
+++ app/utils/viewcontroller.py
@@ -0,0 +1,12 @@
+from lib import View
+
+
+class ViewController:
+    def __init__(self, initial_view) -> None:
+        self.current_view = initial_view
+        self.current_view.show()
+
+    def change_view(self, view: View) -> None:
+        self.current_view.remove()
+        view.show()
+        self.current_view = view
