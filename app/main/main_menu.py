c9de96f218f1c10ce61bb85f87a535fc9da56a57
Gustavo de los Santos <gusleo94@gmail.com>
2023-05-31 00:21:16 +0200

    feat: menu buttons can change current view now

--- app/main/main_menu.py
+++ app/main/main_menu.py
@@ -1,8 +1,10 @@
 import customtkinter as ctk
 
+from lib import View
 from utils import ViewController
 from products import ProductsView
-from lib import View
+from providers import ProvidersView
+from main.homeview import HomeView
 
 
 class MainMenu(ctk.CTkFrame):
@@ -19,10 +21,22 @@ class MainMenu(ctk.CTkFrame):
         self.view_controller = ViewController(ProductsView(self.parent))
 
     def load_view(self, view: View) -> None:
-        pass
+        self.view_controller.change_view(view)
 
     def load_buttons(self) -> None:
-        # TODO: Implementar los botones aquí
-        [ctk.CTkButton(
-            self.menu_btn_frame, text=f"{i+1}", width=40)
-            .pack(pady=5) for i in range(5)]
+        self.products_btn = ctk.CTkButton(
+            self.menu_btn_frame,
+            text="1", width=40,
+            command=lambda: self.load_view(ProductsView(self.parent)))
+        self.home_btn = ctk.CTkButton(
+            self.menu_btn_frame,
+            text="2", width=40,
+            command=lambda: self.load_view(HomeView(self.parent)))
+        self.providers_btn = ctk.CTkButton(
+            self.menu_btn_frame,
+            text="3", width=40,
+            command=lambda: self.load_view(ProvidersView(self.parent)))
+
+        self.products_btn.pack(pady=5)
+        self.home_btn.pack(pady=5)
+        self.providers_btn.pack(pady=5)
