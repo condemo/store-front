70694f0a714f298bd97e1821c0cb2c28f1f23c5e
Gustavo de los Santos <gusleo94@gmail.com>
2023-06-01 00:40:26 +0200

    feat: basic productview design added

--- app/products/product_views.py
+++ app/products/product_views.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from config import TITLE_FONT
 
 
 class ProductsView(ctk.CTkFrame):
@@ -8,6 +9,8 @@ class ProductsView(ctk.CTkFrame):
         self.pack_propagate(False)
 
         self.search_bar = SearchBar(self)
+        self.products_cards_frame = ProductsCardsFrame(self)
+        self.product_info_frame = ProductInfoFrame(self)
 
     def show(self) -> None:
         self.pack(fill="both", expand=True, side="left")
@@ -29,3 +32,36 @@ class SearchBar(ctk.CTkFrame):
         self.search_btn.pack(side="left", padx=5)
         self.container.pack(expand=True)
         self.pack(fill="x")
+
+
+class ProductsCardsFrame(ctk.CTkFrame):
+    def __init__(self, parent) -> None:
+        super().__init__(master=parent)
+        self.pack_propagate(False)
+
+        self.title = ctk.CTkLabel(
+            self, text="Últimos Productos", font=TITLE_FONT)
+
+        self.title.pack(pady=5)
+        self.pack(side="left", expand=True, fill="both")
+
+
+class ProductInfoFrame(ctk.CTkFrame):
+    def __init__(self, parent) -> None:
+        super().__init__(master=parent)
+        self.pack_propagate(False)
+
+        self.title = ctk.CTkLabel(
+            self, text="Información Detallada", font=TITLE_FONT)
+
+        self.title.pack(pady=5)
+        self.pack(side="left", expand=True, fill="both")
+
+
+class ProductCard(ctk.CTkFrame):
+    def __init__(self, parent) -> None:
+        super().__init__(master=parent, fg_color="#4f61dd")
+        self.pack_propagate(False)
+        # TODO: Implementar
+
+        self.pack()
