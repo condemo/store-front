9f29a0b672ed117dc432d8c771958b9df5ef0413
Gustavo de los Santos <gusleo94@gmail.com>
2023-06-02 01:02:30 +0200

    feat: basic productcard implementation

--- app/products/product_views.py
+++ app/products/product_views.py
@@ -43,6 +43,15 @@ class ProductsCardsFrame(ctk.CTkFrame):
             self, text="Últimos Productos", font=TITLE_FONT)
 
         self.title.pack(pady=5)
+        # TODO: Remove Test
+        ProductCard(self)
+        ProductCard(self)
+        ProductCard(self)
+        ProductCard(self)
+        ProductCard(self)
+        ProductCard(self)
+        ProductCard(self)
+
         self.pack(side="left", expand=True, fill="both")
 
 
@@ -60,8 +69,29 @@ class ProductInfoFrame(ctk.CTkFrame):
 
 class ProductCard(ctk.CTkFrame):
     def __init__(self, parent) -> None:
-        super().__init__(master=parent, fg_color="#4f61dd")
+        super().__init__(
+            master=parent, fg_color="#4f61dd", border_width=2,
+            border_color="orange", corner_radius=10)
         self.pack_propagate(False)
-        # TODO: Implementar
 
-        self.pack()
+        self.columnconfigure(0, weight=7, uniform="a")
+        self.columnconfigure(1, weight=4, uniform="a")
+        self.columnconfigure(2, weight=1, uniform="a")
+        self.rowconfigure(0, weight=1, uniform="a")
+        self.rowconfigure(1, weight=1, uniform="a")
+
+        self.name = ctk.CTkLabel(
+            self, text="Titulo provisorio algo largo x si aca a ver",
+            font=("Roboto", 18))
+        self.category = ctk.CTkLabel(
+            self, text="Camisetas",
+            fg_color="transparent", font=("Roboto", 16))
+        self.price = ctk.CTkLabel(self, text="29.99€")
+        self.info_btn = ctk.CTkButton(self, text="->", fg_color="orange")
+
+        self.name.grid(column=0, row=0, rowspan=2)
+        self.category.grid(column=1, row=0, sticky="e", padx=5)
+        self.price.grid(column=1, row=1, sticky="e", padx=5, pady=5)
+        self.info_btn.grid(column=2, row=0, rowspan=2, sticky="nswe")
+
+        self.pack(fill="x", padx=5, pady=3)
